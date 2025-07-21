e315c6b2ab8edd84254f9745a976e6099ec459e6
Isaque Franklin <isaquefrankli@gmail.com>
2025-07-21 20:59:09 -0300

    add: rest of the code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/isaquefranklin/go-crawl-1
 
 go 1.24.5
+
+require golang.org/x/net v0.42.0 // indirect
---
+++ websiteconnector.go
@@ -0,0 +1,31 @@
+package webcrawler
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func ConnectToWebsite(webpageURL string) (*http.Response, bool) {
+	nilResponse := http.Response{}
+	client := http.Client{
+		Timeout: 60 * time.Second,
+	}
+
+	request, err := http.NewRequest("GET", webpageURL, nil)
+	if err != nil {
+		fmt.Println("Received error while creating new request: ", err)
+		return &nilResponse, false
+	}
+
+	request.Header.Set("User-Agent", "Go-Crawl-1 https://github.com/isaquefranklin/go-crawl-1 - This bot retrieves links and content.")
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		fmt.Println("Received error while connecting to website: ", err)
+		return &nilResponse, false
+	}
+
+	return response, true
+}
